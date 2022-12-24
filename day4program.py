a5cb1b90578e21d538268fcf9d7358b791714f43
tarugupradeepkumarreddy <112656850+tarugupradeepkumarreddy@users.noreply.github.com>
2022-12-24 16:15:19 +0530

    Add files via upload

---
+++ day4program.py
@@ -0,0 +1,7 @@
+student=[[43,'pradeep',71,75],[44,'prasad',75,85],[17,'Harika',79,85],[14,'sgouse',78,80]]
+student.sort()
+student.sort(key=lambda temp:temp[2])
+student.sort(key=lambda temp:temp[3])
+print(student)
+print(student)
+print(student)
